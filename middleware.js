67992116068b1051c5722b381da8ee24a19ebd4c
Rachel Murray <dcbeergoddess@gmail.com>
2021-05-11 17:30:05 -0400

    middleware set up to authenticate author for project

--- middleware.js
+++ middleware.js
@@ -1,9 +1,39 @@
+const { projectSchema } = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError');
+const Project = require('./models/project');
+const Comment = require('./models/comment');
+
 module.exports.isLoggedIn = (req, res, next) => {
+  console.log(req.user)
   if(!req.isAuthenticated()) {
     //RETURN USER TO PAGE THEY WERE ON AFTER LOGIN
     req.session.returnTo = req.originalUrl;
     req.flash('error', 'you must be signed in');
     return res.redirect('/login'); 
+  }
+  next();
+};
+//******************************************** */
+//////////////JOI MIDDLEWARE/////////////////////
+//******************************************** */
+//PROJECT VALIDATION
+module.exports.validateProject = (req, res, next) => {
+  //Deconstruct error from response
+  const { error } = projectSchema.validate(req.body);
+  if(error){
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const project = await Project.findById(id);
+  if(!project.author.equals(req.user._id)) {
+    req.flash('error', 'You Do Not Have Permission To Do That!')
+    res.redirect(`/projects/${project._id}`);
   }
   next();
 };
--- routes/projects.js
+++ routes/projects.js
@@ -1,26 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const { projectSchema } = require('../schemas.js')
-const { isLoggedIn } = require('../middleware')
+const { isLoggedIn, validateProject, isAuthor } = require('../middleware')
 
 const ExpressError = require('../utils/ExpressError');
 const Project = require('../models/project');
 
-//******************************************** */
-//////////////JOI MIDDLEWARE/////////////////////
-//******************************************** */
-//PROJECT VALIDATION
-const validateProject = (req, res, next) => {
-  //Deconstruct error from response
-  const { error } = projectSchema.validate(req.body);
-  if(error){
-    const msg = error.details.map(el => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
 //******************************************** */
 ///////////////////ROUTES////////////////////////
 //******************************************** */
@@ -34,7 +19,7 @@ router.get('/new', isLoggedIn, (req, res) => {
   res.render('projects/new')
 });
 //POST NEW PROJECT
-router.post('/', validateProject, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateProject, catchAsync(async (req, res, next) => {
   const project = new Project(req.body.project);
   project.author = req.user._id;
   await project.save();
@@ -43,7 +28,7 @@ router.post('/', validateProject, catchAsync(async (req, res, next) => {
 }));
 //SHOW - PROJECT DETAIL PAGE
 router.get('/:id', catchAsync(async (req, res) => {
-  const project = await (await Project.findById(req.params.id).populate('comments').populate('author'));
+  const project = await Project.findById(req.params.id).populate('comments').populate('author');
   if(!project){
   req.flash('error', 'Cannot Find That Project')
   }
@@ -51,18 +36,23 @@ router.get('/:id', catchAsync(async (req, res) => {
   res.render('projects/show', { project });
 }));
 //EDIT FORM
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
   const project = await Project.findById(req.params.id)
   res.render('projects/edit', { project });
 }));
-//PUT ROUTE
-router.put('/:id', catchAsync(async (req, res) => {
+//PUT ROUTE TO UPDATE
+router.put('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
   const { id } = req.params;
+  const p = await Project.findById(id);
+  if(!p.author.equals(req.user._id)) {
+    req.flash('error', 'You do not have permission to do that');
+    return res.redirect(`/projects/${id}`);
+  }
   const project = await Project.findByIdAndUpdate(id, {...req.body.project}, {new: true});
   res.redirect(`${project._id}`)
 }));
 //DELETE ROUTE
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
   const { id } = req.params;
   await Project.findByIdAndDelete(id);
   res.redirect('/projects');
