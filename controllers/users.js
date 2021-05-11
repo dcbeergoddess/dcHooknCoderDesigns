f7bc27e93829c81e680b3df491058c44c238fc70
Rachel Murray <dcbeergoddess@gmail.com>
2021-05-11 19:39:09 -0400

    user route logic moved to controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,40 @@
 const User = require('../models/user');
 
+module.exports.renderRegisterForm = (req, res) => {
+  res.render('users/register')
+};
+
+module.exports.registerUser = async (req, res) => {
+  try {
+  const { email, username, password } = req.body;
+  const user = new User({ email, username });
+  const registeredUser = await User.register(user, password);
+  // console.log(registeredUser);
+  //KEEP USER LOGGED IN AFTER REGISTERING--> passport helper method `login()`
+  req.login(registeredUser, err => {
+    if(err) return next(err)
+    req.flash('success', 'Welcome to DC Hook & Coder Designs!');
+    res.redirect('/projects');
+  })
+  } catch(e) {
+    req.flash('error', e.message);
+    res.redirect('register');
+  }
+};
+
+module.exports.renderLoginForm = (req, res) => {
+  res.render('users/login')
+};
+
+module.exports.loginUser = (req, res) => {
+  req.flash('success', 'Welcome Back!');
+  const redirectURL = req.session.returnTo || '/projects'
+  delete req.session.returnTo;
+  res.redirect(redirectURL);
+};
+
+module.exports.logoutUser = (req, res) => {
+  req.logout();
+  req.flash('success', "GoodBye!")
+  res.redirect('/projects');
+};
--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const catchAsync = require('../utils/catchAsync')
-const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
+const users = require('../controllers/users');
 
 //RENDER REGISTER FORM
-router.get('/register', (req, res) => {
-  res.render('users/register')
-})
+router.get('/register', users.renderRegisterForm)
 
 //POST USER
-router.post('/register', catchAsync(async (req, res) => {
-  try {
-  const { email, username, password } = req.body;
-  const user = new User({ email, username });
-  const registeredUser = await User.register(user, password);
-  // console.log(registeredUser);
-  //KEEP USER LOGGED IN AFTER REGISTERING--> passport helper method `login()`
-  req.login(registeredUser, err => {
-    if(err) return next(err)
-    req.flash('success', 'Welcome to DC Hook & Coder Designs!');
-    res.redirect('/projects');
-  })
-  } catch(e) {
-    req.flash('error', e.message);
-    res.redirect('register');
-  }
-}));
+router.post('/register', catchAsync(users.registerUser));
 
 //RENDER LOGIN FORM
-router.get('/login', (req, res) => {
-  res.render('users/login')
-});
+router.get('/login', users.renderLoginForm);
 
 //POST LOGIN 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async (req, res) => {
-  req.flash('success', 'Welcome Back!');
-  const redirectURL = req.session.returnTo || '/projects'
-  delete req.session.returnTo;
-  res.redirect(redirectURL);
-});
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.loginUser);
 
 //LOGOUT
-router.get('/logout', (req, res) => {
-  req.logout();
-  req.flash('success', "GoodBye!")
-  res.redirect('/projects');
-});
+router.get('/logout', users.logoutUser);
 
 module.exports = router;
