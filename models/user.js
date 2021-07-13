6a24de2ad1e1f20b8f9e3e8ed5dea6f5fef8081f
Rachel Murray <dcbeergoddess@gmail.com>
2021-07-13 18:35:19 -0400

    deleteing npm package

--- models/user.js
+++ models/user.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
+const passwordComplexity = require('joi-password-complexity');
+passwordComplexity().validate("aPassword123!");
+
 const UserSchema = new Schema({
   email: {
     type: String,
     required: true, 
     unique: true
-  }
+  },
+  password: passwordComplexity().required(),
 });
 
 //THIS WILL ADD username, password to schema and make sure usernames are unique --> and give us some additional methods that we can use
