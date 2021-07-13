d1e85c3020995feca165545629ab8490035e7187
Rachel Murray <dcbeergoddess@gmail.com>
2021-07-13 13:21:30 -0400

    security in place for entering html in text fields

--- schemas.js
+++ schemas.js
@@ -1,4 +1,28 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+//HTML EJS SECURITY - NOT ALLOW HTML IN TEST FIELDS
+const extension = (joi) => ({
+  type: 'string',
+  base: joi.string(),
+  messages: {
+      'string.escapeHTML': '{{#label}} must not include HTML!'
+  },
+  rules: {
+      escapeHTML: {
+          validate(value, helpers) {
+              const clean = sanitizeHtml(value, {
+                  allowedTags: [],
+                  allowedAttributes: {},
+              });
+              if (clean !== value) return helpers.error('string.escapeHTML', { value })
+              return clean;
+          }
+      }
+  }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 //PROJECT VALIDATION
 module.exports.projectSchema = Joi.object ({
